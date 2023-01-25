0c2dffa81b76edff5635fb8281ab39591fe2f122
Harshvardhan Chaturvedi <41356898+harshvardhan96@users.noreply.github.com>
2023-01-25 14:03:38 +0530

    Update server.py

--- server.py
+++ server.py
@@ -12,10 +12,7 @@ def train():
     result = subprocess.call(["python", "train.py", "--resume_ckpt", "sd_v1-5_vae", "--max_epochs", "50", "--data_root", "input",
                               "--lr_scheduler", "cosine", "--project_name", "myproj", "-batch_size", "6", "--sample_steps", "200",
                               "--lr", "3e-6", "--ckpt_every_n_minutes", "10", "--useadam8bit", "--ed1_mode"])
-    if result != 0:
-        print("Error: Script did not run correctly")
-    else:
-        print("Script ran successfully")
+    
     output = result
     print("Output:", output)
     return "Training started"
